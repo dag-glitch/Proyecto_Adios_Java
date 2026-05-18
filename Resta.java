36fd2890bfc474cfdc1251ecacb27d99c08744ba
unknown <dc066971@gmail.com>
2026-05-17 21:55:18 -0600

    creacion de la clase resta

--- Resta.java
+++ Resta.java
@@ -1,9 +1,18 @@
-public class AdiosJava {
+import java.util.Scanner;
+public class Resta {
 
     public static void main(String[] args) {
-        System.out.println("Adios Java");
-        System.out.println("Holaaa");
+        int numero1, numero2, resta;
+        //Ingreso de datos 
+        Scanner scanner = new Scanner (System.in);
+        System.out.println("Ingrese numero 1 ");
+        numero1 = scanner.nextInt();
+        System.out.println("Ingrese numero 2 ");
+        numero2 = scanner.nextInt();
         
 
+        //Operacion 
+        resta = numero1-numero2;
+        System.out.println("El resultado de la resta es: " + resta);
     }
 }
